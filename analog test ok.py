6e4531bcd09c15e84728e924c5f6a664743ed19f
futurecube_git <de-coder@mail.de>
2025-03-01 18:49:24 +0100

    Update analog test ok.py

--- analog test ok.py
+++ analog test ok.py
@@ -12,5 +12,5 @@ ANALOG_PIN = machine.Pin(2, machine.Pin.IN, machine.Pin.PULL_UP)
 
 while True:
     analog_value = ANALOG_PIN.value()
-    print("| Digital State:", "Sound Detected" if analog_value == 1 else "No Sound")
+    print("| Analog State:", "Sound Detected" if analog_value == 1 else "No Sound")
     time.sleep(1)
