800708c4fd4a8d0c6c2e7fcc884658e6f7c0c2fe
futurecube_git <de-coder@mail.de>
2025-03-01 18:44:41 +0100

    Add files via upload

---
+++ analog test ok.py
@@ -0,0 +1,16 @@
+'''
+analog test
+pin A0 --> esp32 gpio2
+Stellsschraube ziemlich ganz rechts, Ausgabe: "No Sound", bei Geräusch "sound detected"
+led02 auf sensor ist aus, bei Geräuscherkennung blinkt er
+'''
+
+import machine
+import time
+
+ANALOG_PIN = machine.Pin(2, machine.Pin.IN, machine.Pin.PULL_UP)
+
+while True:
+    analog_value = ANALOG_PIN.value()
+    print("| Digital State:", "Sound Detected" if analog_value == 1 else "No Sound")
+    time.sleep(1)
